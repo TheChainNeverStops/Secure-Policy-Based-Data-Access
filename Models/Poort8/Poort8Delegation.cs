76737603b40fdba67d0a00e8e1d61cf9ba87a801
TheChainNeverStops <106962681+TheChainNeverStops@users.noreply.github.com>
2022-06-06 18:21:38 +0700

    Create Poort8Delegation.cs

---
+++ Models/Poort8/Poort8Delegation.cs
@@ -0,0 +1,9 @@
+using SecurePolicyBasedDataAccess.Models.Playbook;
+
+namespace SecurePolicyBasedDataAccess.Models
+{
+    public class Poort8Delegation
+    {
+        public PlaybookDelegationEvidence DelegationRequest { get; set; }
+    }
+}
