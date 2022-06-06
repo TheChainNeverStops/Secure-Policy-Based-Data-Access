6cc041d68a622559bd852171f063f8921b4f778b
TheChainNeverStops <106962681+TheChainNeverStops@users.noreply.github.com>
2022-06-06 17:49:22 +0700

    Create EpochTimeExtensions.cs

---
+++ Infrastructure/Extensions/EpochTimeExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+
+namespace SecurePolicyBasedDataAccess.Infrastructure.Extensions
+{
+    public static class EpochTimeExtensions
+    {
+        public static string ToEpoch(this DateTime value)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var elapsedTime = value - epoch;
+            return ((long)elapsedTime.TotalSeconds).ToString(CultureInfo.CurrentCulture);
+        }
+
+        public static long ToEpochNumber(this DateTime value)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var elapsedTime = value - epoch;
+            return (long)elapsedTime.TotalSeconds;
+        }
+    }
+}
