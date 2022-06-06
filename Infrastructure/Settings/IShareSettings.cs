328cc5f3864705aecc040cd770ccb5c05b0b7c2c
TheChainNeverStops <106962681+TheChainNeverStops@users.noreply.github.com>
2022-06-06 18:38:35 +0700

    Update IShareSettings.cs

--- Infrastructure/Settings/IShareSettings.cs
+++ Infrastructure/Settings/IShareSettings.cs
@@ -5,7 +5,7 @@ namespace SecurePolicyBasedDataAccess.Infrastructure.Settings
         public string Host { get; set; }
         public string LinkPolicy { get; set; }
         public string LinkDelegationEvidence { get; set; }
-        public string TargetAudience { get; set; } = "EU.EORI.NL888888881";
+        public string TargetAudience { get; set; } = "EU.EORI. Your Audience";
         public string UrlSchemeAuthorize { get; set; } = "testing/generate-authorize-request";
         public string UrlPoort8GetToken { get; set; } = "ar-preview/ishare/connect/token";
         public string ConnectionStrings { get; set; }
