3c1bcae8ed5bb45d5337a065b5349c4285dfd7de
TheChainNeverStops <106962681+TheChainNeverStops@users.noreply.github.com>
2022-07-11 17:45:30 +0700

    Update AccessTokensController.cs

--- Controllers/AccessTokensController.cs
+++ Controllers/AccessTokensController.cs
@@ -24,11 +24,41 @@
                 string urlSchemas = _settings.UrlSchemeAuthorize;
                 string urlGetToken = _settings.UrlPoort8GetToken;
 
+                if (string.IsNullOrEmpty(privateKey))
+                {
+                    return Ok(new
+                    {
+                        isError = true,
+                        ErrorMsg = $"Private key is not correct, please try again later."
+                    });
+                }
+
+                if (string.IsNullOrEmpty(publicKey))
+                {
+                    return Ok(new
+                    {
+                        isError = true,
+                        ErrorMsg = $"Public key is not correct, please try again later."
+                    });
+                }
+                
                 publicKey = publicKey.Replace("-----BEGIN CERTIFICATE-----", "");
                 publicKey = publicKey.Replace("-----END CERTIFICATE-----", "");
                 publicKey = publicKey.Replace("\r\n", "");
                 publicKey = publicKey.Replace(" ", "");
 
+                //Verify iSHARE ID with Certificate
+                var signingCertificate = new X509Certificate2(Convert.FromBase64String(publicKey));
+                string subject = signingCertificate.Subject;
+                if (string.IsNullOrEmpty(subject) || !subject.Contains(myIdentifier))
+                {
+                    return Ok(new
+                    {
+                        isError = true,
+                        ErrorMsg = $"PublicKey is not correct with your Identifier, please try again later."
+                    });
+                }
+                
                 string clientAssertion = await tokenService.GetTokenFromSchemaAsync(audParty,
                     issParty, privateKey, publicKey, urlSchemas);
 
