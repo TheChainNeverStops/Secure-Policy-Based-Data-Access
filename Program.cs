102f2a52930b4a020426b99a44e7b9b88967695a
TheChainNeverStops <106962681+TheChainNeverStops@users.noreply.github.com>
2022-07-12 14:17:06 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,3 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SecurePolicyBasedDataAccess
-{
     public class Program
     {
         public static void Main(string[] args)
@@ -23,4 +12,3 @@ namespace SecurePolicyBasedDataAccess
                     webBuilder.UseStartup<Startup>();
                 });
     }
-}
